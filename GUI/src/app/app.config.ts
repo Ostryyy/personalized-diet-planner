ff4a3299304f829a748714dec9f61ff360490794
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2025-01-05 00:05:28 +0100

    reconfigured angular project due to errors

--- GUI/src/app/app.config.ts
+++ GUI/src/app/app.config.ts
@@ -1,25 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { importProvidersFrom } from "@angular/core";
-import { routes } from "./app.routes";
-import { ReactiveFormsModule } from "@angular/forms";
-import { ToastrModule } from "ngx-toastr";
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAnimations(),
-    importProvidersFrom(
-      ReactiveFormsModule,
-      ToastrModule.forRoot({
-        timeOut: 3000,
-        positionClass: "toast-bottom-right",
-        preventDuplicates: true,
-      })
-    ),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
 };
