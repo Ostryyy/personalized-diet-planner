431c84c7cf6af901ad16ac7b4de2de359065df8c
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2025-01-11 00:35:23 +0100

    API: moved some tests to the playwright configuration file

--- API/tests/user.spec.js
+++ API/tests/user.spec.js
@@ -1,34 +1,15 @@
 import { test, expect } from "@playwright/test";
+import fs from "fs";
 
-const BASE_URL = "http://localhost:82";
+let token;
+test.beforeAll(() => {
+  const authData = JSON.parse(fs.readFileSync("auth-token.json", "utf-8"));
+  token = authData.token;
+});
 
-test.describe("User API tests", () => {
-  let token;
-
-  test.beforeAll(async ({ request }) => {
-    await request.post(`${BASE_URL}/api/users/register`, {
-      data: {
-        username: "test_user",
-        firstName: "api",
-        lastName: "test_user",
-        email: "testuser@example.com",
-        password: "password123",
-      },
-    });
-
-    const loginResponse = await request.post(`${BASE_URL}/api/users/login`, {
-      data: {
-        email: "testuser@example.com",
-        password: "password123",
-      },
-    });
-    expect(loginResponse.status()).toBe(200);
-    const loginBody = await loginResponse.json();
-    token = loginBody.token;
-  });
-
-  test("Get user profile with token", async ({ request }) => {
-    const response = await request.get(`${BASE_URL}/api/users/profile`, {
+test.describe("User Controller API Tests", () => {
+  test("Get user profile", async ({ request }) => {
+    const response = await request.get("/api/users/profile", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -38,8 +19,8 @@ test.describe("User API tests", () => {
     expect(responseBody).toHaveProperty("username", "test_user");
   });
 
-  test("Update user profile with token", async ({ request }) => {
-    const response = await request.put(`${BASE_URL}/api/users/profile`, {
+  test("Update user profile", async ({ request }) => {
+    const response = await request.put("/api/users/profile", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -52,7 +33,6 @@ test.describe("User API tests", () => {
         activityLevel: "moderate",
       },
     });
-
     expect(response.status()).toBe(200);
     const responseBody = await response.json();
     expect(responseBody.weight).toBe(75);
@@ -60,66 +40,23 @@ test.describe("User API tests", () => {
     expect(responseBody.goal).toBe("lose");
   });
 
-  test("Update user weight with token", async ({ request }) => {
-    const newWeight = 80;
-
-    const response = await request.put(`${BASE_URL}/api/users/weight`, {
+  test("Update user weight", async ({ request }) => {
+    const response = await request.put("/api/users/weight", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
       data: {
-        weight: newWeight,
+        weight: 80,
       },
     });
-
     expect(response.status()).toBe(200);
 
     const responseBody = await response.json();
-
     expect(responseBody.message).toBe("Weight updated successfully");
 
     const lastEntry =
       responseBody.weightHistory[responseBody.weightHistory.length - 1];
-    expect(lastEntry.weight).toBe(newWeight);
-
+    expect(lastEntry.weight).toBe(80);
     expect(new Date(lastEntry.date)).not.toBeNaN();
-  });
-
-  test("Register an existing user", async ({ request }) => {
-    const response = await request.post(`${BASE_URL}/api/users/register`, {
-      data: {
-        username: "test_user",
-        firstName: "api",
-        lastName: "test_user",
-        email: "testuser@example.com",
-        password: "password123",
-      },
-    });
-
-    expect(response.status()).toBe(400);
-    const responseBody = await response.json();
-    expect(responseBody.message).toBe("User already exists");
-  });
-
-  test("Login with invalid password", async ({ request }) => {
-    const response = await request.post(`${BASE_URL}/api/users/login`, {
-      data: {
-        email: "testuser@example.com",
-        password: "wrongpassword",
-      },
-    });
-
-    expect(response.status()).toBe(401);
-    const responseBody = await response.json();
-    expect(responseBody.message).toBe("Invalid email or password");
-  });
-
-  test("Delete user account with token", async ({ request }) => {
-    const response = await request.delete(`${BASE_URL}/api/users/delete`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    expect(response.status()).toBe(204);
   });
 });
